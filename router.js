6178bfbc453e406d44d7d6ef7992f17c9d74ec3b
noorjadzil <noor.jadzil@gmail.com>
2026-05-22 20:52:33 +0700

    Delete router.js

--- router.js
+++
@@ -1,6 +0,0 @@
-export const APP_CONFIG = {
-  appName: 'Kasir PWA',
-  currency: 'Rp',
-  waNumber: '', // contoh: 6281234567890
-  firebaseUrl: '' // isi nanti jika mau sync: https://PROJECT.firebaseio.com
-};
